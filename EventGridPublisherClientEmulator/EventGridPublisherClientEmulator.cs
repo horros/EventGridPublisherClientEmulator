c921ab10954c12dce4eec6346cf3a059c543cfab
horros <markuslervik1234@gmail.com>
2021-01-26 21:30:16 +0200

    Initial commit

---
+++ EventGridPublisherClientEmulator/EventGridPublisherClientEmulator.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Core.Pipeline;
+using Azure.Messaging.EventGrid;
+
+namespace EventGridPublisherClientEmulator
+{
+    public class ClientEmulator : EventGridPublisherClient
+    {
+
+        private readonly HttpClient client;
+        private readonly Uri responseUri;
+        private readonly AzureKeyCredential credential;
+
+        public ClientEmulator(Uri responseUri)
+        {
+            client = new HttpClient();            
+            this.responseUri = responseUri;
+        }
+
+        public ClientEmulator(Uri responseUri, AzureKeyCredential credential)
+        {
+            client = new HttpClient();
+            this.responseUri = responseUri;
+            this.credential = credential;
+        }
+
+        /// <summary>
+        /// Constructor that takes an extra HttpClient parameter,
+        /// mainly for mocking in tests
+        /// </summary>
+        /// <param name="responseUri"></param>
+        /// <param name="credentials"></param>
+        /// <param name="client"></param>
+        public ClientEmulator(Uri responseUri, AzureKeyCredential credentials, HttpClient client)
+        {
+            this.responseUri = responseUri;
+            this.client = client;
+        }
+
+        // We don't support asynchronous methods (yet)
+        public override Response SendEvents(IEnumerable<EventGridEvent> events, CancellationToken cancellationToken = default) 
+        {
+            throw new Exception("Synchronous calls not implemented");
+        }
+
+        public override Response SendEvents(IEnumerable<CloudEvent> events, CancellationToken cancellationToken = default)
+        {
+            throw new Exception("Synchronous calls not implemented");
+        }
+
+        public override Response SendEvents(IEnumerable<object> events, CancellationToken cancellationToken = default)
+        {
+            throw new Exception("Synchronous calls not implemented");
+        }
+
+        /// <summary>
+        /// Send a collection of EventGridEvents
+        /// </summary>
+        /// <param name="eventGridEvents"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public override async Task<Response> SendEventsAsync(IEnumerable<EventGridEvent> eventGridEvents, CancellationToken cancellationToken = default)
+        {
+
+            // This is a bit wonky, Data is not a publicly accessible field
+            // in EventGridEvent, you can fetch data by calling GetData on the event.
+            // Thus, we need to (re)construct the JSON payload ourselves. Fun!
+            List<Dictionary<string,object>> events = new List<Dictionary<string,object>>();
+
+            foreach (var gridEvent in eventGridEvents)
+            {
+                Dictionary<string, object> tempEvent = new Dictionary<string, object>();
+                tempEvent.Add("Id", gridEvent.Id);
+                tempEvent.Add("Subject", gridEvent.Subject);
+                tempEvent.Add("DataVersion", gridEvent.DataVersion);
+                tempEvent.Add("EventTime", gridEvent.EventTime);
+                tempEvent.Add("EventType", gridEvent.EventType);
+                tempEvent.Add("Data", gridEvent.GetData());
+                events.Add(tempEvent);
+            }
+
+
+            var content = JsonSerializer.Serialize(events);
+            var buffer = System.Text.Encoding.UTF8.GetBytes(content);
+            var byteData = new ByteArrayContent(buffer);
+            
+            // Add the required headers
+            byteData.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            byteData.Headers.Add("aeg-event-type", "Notification");
+            byteData.Headers.Add("aeg-sas-key", credential.Key);
+            
+            var result =  await client.PostAsync(responseUri, byteData);
+
+            return new FakeResponse(result);
+        }
+
+        public override Task<Response> SendEventsAsync(IEnumerable<CloudEvent> events, CancellationToken cancellationToken = default)
+        {
+            throw new Exception("CloudEvents are not supported");
+        }
+
+        public override Task<Response> SendEventsAsync(IEnumerable<object> events, CancellationToken cancellationToken = default)
+        {
+            throw new Exception("Custom events are not supported");
+        }
+
+    }
+
+    /// <summary>
+    /// Class that wraps the abstract Response class that EventGridPublisherClient 
+    /// returns. Just take the HttpResponseMessage give to us by HttpClient 
+    /// and copy the data.
+    /// </summary>
+    internal class FakeResponse : Response
+    {
+        readonly List<HttpHeader> headers = new List<HttpHeader>();
+
+        public FakeResponse(HttpResponseMessage responseMessage)
+        {
+            foreach (var header in responseMessage.Headers)
+            {
+                foreach (var value in header.Value)
+                {
+                    headers.Add(new HttpHeader(header.Key, value));
+                }
+                
+            }
+            Status = (int)responseMessage.StatusCode;
+            ReasonPhrase = responseMessage.ReasonPhrase;
+            ContentStream = Task.Run(async () => await responseMessage.Content.ReadAsStreamAsync()).Result;
+        }
+
+        public override string ReasonPhrase { get; }
+        public override Stream ContentStream  { get; set;}
+        public override string ClientRequestId { get; set; }
+
+        public override int Status { get; }
+
+        public override void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool ContainsHeader(string name)
+        {
+            return headers.Exists(h => h.Name == name);
+        }
+
+        protected override IEnumerable<HttpHeader> EnumerateHeaders()
+        {
+            return headers;
+        }
+
+        protected override bool TryGetHeader(string name, [NotNullWhen(true)] out string value)
+        {
+            if (headers.Exists(h => h.Name == name))
+            {
+                value = headers.Find(h => h.Name == name).Value;
+                return true;
+            } 
+            else
+            {
+                value = null;
+                return false;
+            }
+            
+        }
+
+        protected override bool TryGetHeaderValues(string name, [NotNullWhen(true)] out IEnumerable<string> values)
+        {
+            if (headers.Exists(h => h.Name == name))
+            {
+                List<string> outlist = new List<string>();
+
+                foreach (var header in headers.FindAll(h => h.Name == name))
+                {
+                    outlist.Add(header.Value);
+                }
+                values = outlist;
+                return true;
+            }
+            else
+            {
+                values = new List<string>();
+                return false;
+                
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,64 @@
+# EventGridPublisherClientEmulator
+
+This is a class that "emulates" EventGridPublisherClient in the new Azure.Messaging.EventGrid 4.0.0-beta release.
+
+## Problem
+
+Developing locally with EventGrid is possible, using something like ngrok to open a tunnel from the outside and adding a new Event Subscription to EventGrid pointing to the ngrok tunnel. This is less than ideal in many cases, especially when developing larger event driven microservices apps.
+
+## Solution
+
+Paul Mcilreavy developed the awesome [EventGridSimulator](https://github.com/pmcilreavy/AzureEventGridSimulator) that can act as Azure EventGrid on your local computer. However, try as I may, I could not get the EventGridPublisherClient in the EventGrid 4.0.0-beta4 API to play ball, it kept crashing with an error saying "The SSL connection could not be established" and nothing more of use. I tried everything I could think of at the time, even constructing a new HttpClientHandler for which I disabled certificate validation completely and used that as parameter for a new HttpClient which was used as a parameter for a new HttpClientTransport which was set as the transport on a EventGridPublisherClientOptions object that was passed to EventGridPublisherClient, but nothing worked.
+
+I know EventGridPublisherClient does stuff like force the protocol to be HTTPS regardless of what you pass in as the URI and override the query path etc, so I spent a little while writing together an "emulator" of sorts for EventGridPublisherClient. Since EventGridSimulator does not support CloudEvents I didn't implement support for it in the emulator either, though this should be trivial to add. All the "emulator" does is override
+```csharp
+async Task<Response> SendEventsAsync(IEnumerable<EventGridEvent> eventGridEvents, CancellationToken cancellationToken = default)
+```
+and construct a JSON representation of the events, set the correct headers and post that to the URI specified, in this case EventGridSimulator. Using this you could roll your own local Event Grid-y thing if you wish, but I warmly recommed EventGridSimulator.
+
+## Usage
+
+My quickly cobbled together thing registers a singleton service of type EventGridPublisherClient in `Startup.cs`, except if the environment is Development, we instantiate a ClientEmulator class instead of the EventGridPublisherClient:
+
+```csharp
+    if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
+    {
+        services.AddSingleton<EventGridPublisherClient>(clientEmulator =>
+        {
+            return new ClientEmulator(new Uri("https://localhost:60102/api/events?api-version=2018-01-01"), new AzureKeyCredential("DummyKey"));
+        }); 
+    }
+    else
+    {
+        string topicEndpoint = Environment.GetEnvironmentVariable("EVENT_GRID_TOPIC_ENDPOINT");
+        string key = Environment.GetEnvironmentVariable("EVENT_GRID_KEY");
+        services.AddSingleton<EventGridPublisherClient>(clientEmulator =>
+        {
+            return new EventGridPublisherClient(new Uri(topicEndpoint), new AzureKeyCredential(key));
+        });
+    }
+```
+
+Then I do something like this in my MediatR Notification Handler:
+
+```csharp
+public class ExampleHandler : INotificationHandler<ExampleEvent>
+{
+    private readonly EventGridPublisherClient client;
+
+    public ExampleHandler(EventGridPublisherClient client) {
+        this.client = client;
+    }
+    
+    public async Task Handle(ExampleEvent notification, CancellationToken cancellationToken = default) {
+        await client.SendEventsAsync(GetEventsList(notification), cancellationToken);
+    }
+
+    static IList<EventGridEvent> GetEventsList(ExampleEvent notification) 
+    {
+        // Get a list of events etc
+    }
+}
+```
+
+For any questions, comments or improvements, open an issue.
